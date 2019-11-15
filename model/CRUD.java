3cef5b254deed793767182cdac9ff73ba899624b
Kaio <kaiomudkt@gmail.com>
2019-11-15 12:44:15 -0400

    alterando interface crud do model

--- model/CRUD.java
+++ model/CRUD.java
@@ -9,15 +9,15 @@ import dao.DAOFabricante;
  * @author kaio
  */
 public interface CRUD{
-    public abstract void salvar() throws SQLException;
+    public void salvar() throws SQLException;
 
-    public abstract void atualizar() throws SQLException;
+    public void atualizar() throws SQLException;
 
-    public abstract void deletar() throws SQLException;
+    public void deletar() throws SQLException;
 
-    public abstract Object getById(int id) throws SQLException;//retorna ArrayList<Object>  
+    public Object getById(int id) throws SQLException;//retorna ArrayList<Object>  
 
     //public Object getAllById(int id) throws SQLException;//retorna Object
     //public Object getById(int id, int id) throws SQLException ;
-    public abstract List<Object> getAll() throws SQLException;// pega tudo de uma tabela
+    public List<Object> getAll() throws SQLException;// pega tudo de uma tabela
 }
