490fecc0b533ff572fe0408f45e9667f137b578d
Yan <yanajiki2010@hotmail.com>
2019-11-15 16:25:05 -0400

    Added ID. Waiting for DAO.

--- model/ModelCategoria.java
+++ model/ModelCategoria.java
@@ -5,15 +5,25 @@ import dao.DAOCategoria;
 
 public class ModelCategoria{
     private String nome;
-
-
-    public ModelCategoria(String nome) {
-        this.nome = nome;
-    }
-
+    private int id;
 
     public ModelCategoria() {
     }
+
+    public ModelCategoria(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 
     public String getNome() {
         return this.nome;
@@ -44,9 +54,10 @@ public class ModelCategoria{
 
 
     //acesso DAO
+    private DAOCategoria dao = new DAOCategoria();
     @Override
     public void salvar() throws SQLException {
-        daoEstoque.salvar(this);
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 
